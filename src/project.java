7898bf7f8c7a333df0f6e240aa82837f4f25214e
Marvin Salazar Jr <msalazar42@me.com>
2015-01-05 16:39:16 -0800

    finished part1-3

--- src/project.java
+++ src/project.java
@@ -8,6 +8,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.Exception;import java.lang.String;import java.lang.StringBuilder;
 
+
+
+
+
 //import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.HttpResponse;
@@ -16,6 +20,10 @@ import org.apache.http.client.HttpClient;
 //import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.entity.StringEntity;
+import org.joda.time.DateTime;
+import org.joda.time.MutableDateTime;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class project {
@@ -222,6 +230,7 @@ public class project {
        }
         
 	}
+
 	public static JSONObject haystack(String token)
 	{
 		InputStream inputStream = null;
@@ -292,11 +301,282 @@ public class project {
         return dict;
 	}
 
-	public static void main(String[] args) {
+	public static void validateneedle(String token, int needle)
+	{
+		InputStream inputStream = null;
+        String result;
+        StringBuilder builder = new StringBuilder();
+        String valid = "";
+        try 
+        {    
+          // 1. create HttpClient
+           HttpClient httpclient = new DefaultHttpClient();
+
+           // 2. make POST request to the given URL
+           HttpPost httpPost = new HttpPost("http://challenge.code2040.org/api/validateneedle");
+
+           String json = "";
+
+           // 3. build jsonObject
+           JSONObject jsonObject = new JSONObject();
+           jsonObject.accumulate("token", token);
+           jsonObject.accumulate("needle", needle);
+
+           // 4. convert JSONObject to JSON to String
+           json = jsonObject.toString();
+
+           // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+           // ObjectMapper mapper = new ObjectMapper();
+           // json = mapper.writeValueAsString(person);
+
+           // 5. set json to StringEntity
+           StringEntity se = new StringEntity(json);
+
+           // 6. set httpPost Entity
+           httpPost.setEntity(se);
+
+           // 7. Set some headers to inform server about the type of the content
+           httpPost.setHeader("Accept", "application/json");
+           httpPost.setHeader("Content-type", "application/json");
+
+
+           // 8. Execute POST request to the given URL
+           HttpResponse httpResponse = httpclient.execute(httpPost);
+
+           // 9. receive response as inputStream
+           inputStream = httpResponse.getEntity().getContent();
+
+           BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+           
+           //retrieves string from json response
+           while ((result = reader.readLine()) != null) {
+               builder.append(result);
+           }
+           //System.out.print(builder.toString());
+           valid = builder.toString();
+           //System.out.print(valid);
+
+       } catch (ClientProtocolException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } catch (Exception ex) {
+         ex.printStackTrace();
+       }
+        
+	}
+	
+	public static JSONObject getPrefix(String token)
+	{
+		InputStream inputStream = null;
+        String result;
+        StringBuilder builder = new StringBuilder();
+        String dictionary = "";
+        JSONObject dict = new JSONObject();
+        try 
+        {    
+          // 1. create HttpClient
+           HttpClient httpclient = new DefaultHttpClient();
+
+           // 2. make POST request to the given URL
+           HttpPost httpPost = new HttpPost("http://challenge.code2040.org/api/prefix");
+
+           String json = "";
+
+           // 3. build jsonObject
+           JSONObject jsonObject = new JSONObject();
+           jsonObject.accumulate("token", token);
+
+
+           // 4. convert JSONObject to JSON to String
+           json = jsonObject.toString();
+
+           // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+           // ObjectMapper mapper = new ObjectMapper();
+           // json = mapper.writeValueAsString(person);
+
+           // 5. set json to StringEntity
+           StringEntity se = new StringEntity(json);
+
+           // 6. set httpPost Entity
+           httpPost.setEntity(se);
+
+           // 7. Set some headers to inform server about the type of the content
+           httpPost.setHeader("Accept", "application/json");
+           httpPost.setHeader("Content-type", "application/json");
+
+
+           // 8. Execute POST request to the given URL
+           HttpResponse httpResponse = httpclient.execute(httpPost);
+
+           // 9. receive response as inputStream
+           inputStream = httpResponse.getEntity().getContent();
+
+           BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+           
+           //retrieves string from json response
+           while ((result = reader.readLine()) != null) {
+               builder.append(result);
+           }
+           //System.out.print(builder.toString());
+           dictionary = builder.toString();
+           int i = dictionary.indexOf(':');
+           dictionary = dictionary.substring(i+1, dictionary.length()-1);
+           //System.out.print(dictionary);
+         
+           dict = new JSONObject(dictionary);
+           
+       } catch (ClientProtocolException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } catch (Exception ex) {
+         ex.printStackTrace();
+       }
+        return dict;
+	}
+	
+	public static void validateprefix(String token, String[] array)
+	{
+		InputStream inputStream = null;
+        String result;
+        StringBuilder builder = new StringBuilder();
+        String valid = "";
+        try 
+        {    
+          // 1. create HttpClient
+           HttpClient httpclient = new DefaultHttpClient();
+
+           // 2. make POST request to the given URL
+           HttpPost httpPost = new HttpPost("http://challenge.code2040.org/api/validateprefix");
+
+           String json = "";
+
+           // 3. build jsonObject
+           JSONObject jsonObject = new JSONObject();
+           jsonObject.accumulate("token", token);
+           jsonObject.accumulate("array", array);
+
+           // 4. convert JSONObject to JSON to String
+           json = jsonObject.toString();
+
+           // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+           // ObjectMapper mapper = new ObjectMapper();
+           // json = mapper.writeValueAsString(person);
+
+           // 5. set json to StringEntity
+           StringEntity se = new StringEntity(json);
+
+           // 6. set httpPost Entity
+           httpPost.setEntity(se);
+
+           // 7. Set some headers to inform server about the type of the content
+           httpPost.setHeader("Accept", "application/json");
+           httpPost.setHeader("Content-type", "application/json");
+
+
+           // 8. Execute POST request to the given URL
+           HttpResponse httpResponse = httpclient.execute(httpPost);
+
+           // 9. receive response as inputStream
+           inputStream = httpResponse.getEntity().getContent();
+
+           BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+           
+           //retrieves string from json response
+           while ((result = reader.readLine()) != null) {
+               builder.append(result);
+           }
+           //System.out.print(builder.toString());
+           valid = builder.toString();
+           //System.out.print(valid);
+
+       } catch (ClientProtocolException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } catch (Exception ex) {
+         ex.printStackTrace();
+       }
+        
+	}
+	
+	public static JSONObject getTime(String token)
+	{
+		InputStream inputStream = null;
+        String result;
+        StringBuilder builder = new StringBuilder();
+        String dictionary = "";
+        JSONObject dict = new JSONObject();
+        try 
+        {    
+          // 1. create HttpClient
+           HttpClient httpclient = new DefaultHttpClient();
+
+           // 2. make POST request to the given URL
+           HttpPost httpPost = new HttpPost("http://challenge.code2040.org/api/time");
+
+           String json = "";
+
+           // 3. build jsonObject
+           JSONObject jsonObject = new JSONObject();
+           jsonObject.accumulate("token", token);
+
+
+           // 4. convert JSONObject to JSON to String
+           json = jsonObject.toString();
+
+           // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+           // ObjectMapper mapper = new ObjectMapper();
+           // json = mapper.writeValueAsString(person);
+
+           // 5. set json to StringEntity
+           StringEntity se = new StringEntity(json);
+
+           // 6. set httpPost Entity
+           httpPost.setEntity(se);
+
+           // 7. Set some headers to inform server about the type of the content
+           httpPost.setHeader("Accept", "application/json");
+           httpPost.setHeader("Content-type", "application/json");
+
+
+           // 8. Execute POST request to the given URL
+           HttpResponse httpResponse = httpclient.execute(httpPost);
+
+           // 9. receive response as inputStream
+           inputStream = httpResponse.getEntity().getContent();
+
+           BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+           
+           //retrieves string from json response
+           while ((result = reader.readLine()) != null) {
+               builder.append(result);
+           }
+           //System.out.print(builder.toString());
+           dictionary = builder.toString();
+           int i = dictionary.indexOf(':');
+           dictionary = dictionary.substring(i+1, dictionary.length()-1);
+           System.out.print(dictionary);
+         
+           dict = new JSONObject(dictionary);
+           
+       } catch (ClientProtocolException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       } catch (Exception ex) {
+         ex.printStackTrace();
+       }
+        return dict;
+	}
+	
+	public static void main(String[] args) throws JSONException {
 		
 		//part0:Registration
         String token = getToken();
         
+        //-------------------------------------------------------------
         
         //partI: reverse a string
         String string2rev = getstring(token);
@@ -311,11 +591,80 @@ public class project {
         
         validatestring(token, rev);
         
+        //---------------------------------------------------
+        
         //partII: needle in a haystack
         JSONObject dictionary = haystack(token);
         //retrieve array and needle find needle in array.
-        System.out.print(dictionary.toString());
-          
+        
+        String needle = dictionary.getString("needle");
+        JSONArray hay = dictionary.getJSONArray("haystack");
+        
+        int location = -1;
+        
+        for(int j = 0; j < hay.length(); j++)
+        {
+        	if(needle.compareTo(hay.getString(j)) == 0)
+        	{
+        		location = j;
+        	}
+        }
+        
+        validateneedle(token, location);
+        
+        //---------------------------------------------------
+        
+        //partIII: prefixes
+        JSONObject dict2 = getPrefix(token);
+        
+        String prefix = dict2.getString("prefix");
+        JSONArray array = dict2.getJSONArray("array");
+        JSONArray array2 = new JSONArray();
+        String temp = "";
+
+        for(int k = 0; k < array.length(); k++)
+        {
+        	temp = array.getString(k);
+        	if(!temp.startsWith(prefix))
+        	{
+        		array2.put(temp);
+        	}
+        }
+        String[] array3 = new String[array2.length()];
+        for(int q = 0; q < array2.length(); q++)
+        {
+        	array3[q] = array2.getString(q);
+        }
+        
+        validateprefix(token, array3);
+        
+        //----------------------------------------------------------------
+        
+        //PartIV: the dating game
+        JSONObject dict4 = getTime(token);
+        String datestamp = dict4.getString("datestamp");
+        int interval = dict4.getInt("interval");
+        
+        int sec = interval%60;
+        int min = (interval/60)%60;
+        int hr = (interval/3600)%24;
+        int day = (interval/86400)%365;
+        int year = (interval/315360000);
+        
+        MutableDateTime datetime = new MutableDateTime(datestamp);
+        datetime.addSeconds(sec);
+        datetime.addMinutes(min);
+        datetime.addHours(hr);
+        datetime.addDays(day);
+        datetime.addYears(year);
+        
+        System.out.print(year + "\n"+ day + "\n" + hr + "\n" + min + "\n" + sec);
+        
+        
+        String newdate = datetime.toString(); 
+        System.out.print("\n" + newdate);
+        
+        
 	}
 
 }
